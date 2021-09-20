77f480a37ac5ea98221169600e8448bcd9e67b75
Johan <johanleo97@gmail.com>
2021-09-20 11:42:17 -0600

    cambio puerto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const { response } = require('express');
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
